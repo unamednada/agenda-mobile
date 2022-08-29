1a803cd10047ab8f9e59042ed98442bb5ec4b52c
unamednada <dias.gustavo.dias@gmail.com>
2022-08-29 14:40:58 -0300

    feat: logic of validation in login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, TextInput } from '../../components';
+import Verify from '../../utils/Verify';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <div>
       <div>
@@ -12,16 +16,16 @@ export default function Login() {
           <p>Acesse sua conta agora mesmo</p>
           <TextInput
             placeholder="Email:"
-            value=""
-            onChange={() => {}}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <TextInput
             placeholder="senha:"
-            value=""
-            onChange={() => {}}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             password
           />
-          <Button text="Entrar" onClick={() => {}} disabled />
+          <Button text="Entrar" onClick={() => {}} disabled={ !(Verify.isEmail(email) && Verify.isPassword(password)) } />
           <a href="/register">registrar-se</a>
         </div>
       </form>
