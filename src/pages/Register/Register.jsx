47c37d20fe560047621761fb2a616931b28077c7
unamednada <dias.gustavo.dias@gmail.com>
2022-08-30 07:33:28 -0300

    feat: add value and handlechange to fields in register

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, TextInput } from '../../components';
 
 export default function Register() {
+  const [student, setStudent] = useState('');
+
+  const studentObj = {
+    name: '',
+    email: '',
+    password: '',
+    cpf: '',
+    city: '',
+    address: '',
+    cep: '',
+    campus: '',
+    mode: '',
+    area: '',
+    course: '',
+  };
+
   return (
     <div>
       <div>
@@ -9,22 +25,72 @@ export default function Register() {
       </div>
       <form>
         <p>Preencher todos os campos abaixo:</p>
-        <TextInput placeholder="Nome:" />
-        <TextInput placeholder="Email:" />
-        <TextInput placeholder="Confirmar email:" />
+        <TextInput
+          placeholder="Nome:"
+          value={studentObj.name}
+          onChange={(e) => setStudent({ ...studentObj, name: e.target.value })}
+        />
+        <TextInput
+          placeholder="Email:"
+          value={studentObj.email}
+          onChange={(e) => setStudent({ ...studentObj, email: e.target.value })}
+        />
+        <TextInput
+          placeholder="Confirmar email:"
+        />
         <p>senha:</p>
-        <TextInput placeholder="senha:" password />
-        <TextInput placeholder="Confirmar senha:" password />
+        <TextInput
+          placeholder="senha:"
+          value={studentObj.password}
+          onChange={(e) => setStudent({ ...studentObj, password: e.target.value })}
+          password
+        />
+        <TextInput
+          placeholder="Confirmar senha:"
+          password
+        />
         <p>Seus dados:</p>
-        <TextInput placeholder="CPF:" />
-        <TextInput placeholder="Cidade:" />
-        <TextInput placeholder="Endereço:" />
-        <TextInput placeholder="CEP:" />
+        <TextInput
+          placeholder="CPF:"
+          value={studentObj.cpf}
+          onChange={(e) => setStudent({ ...studentObj, cpf: e.target.value })}
+        />
+        <TextInput
+          placeholder="Cidade:"
+          value={studentObj.city}
+          onChange={(e) => setStudent({ ...studentObj, city: e.target.value })}
+        />
+        <TextInput
+          placeholder="Endereço:"
+          value={studentObj.address}
+          onChange={(e) => setStudent({ ...studentObj, address: e.target.value })}
+        />
+        <TextInput
+          placeholder="CEP:"
+          value={studentObj.cep}
+          onChange={(e) => setStudent({ ...studentObj, cep: e.target.value })}
+        />
         <p>Curso:</p>
-        <TextInput placeholder="Unidade Polo:" />
-        <TextInput placeholder="Modalidade:" />
-        <TextInput placeholder="Área:" />
-        <TextInput placeholder="Curso:" />
+        <TextInput
+          placeholder="Unidade Polo:"
+          value={studentObj.campus}
+          onChange={(e) => setStudent({ ...studentObj, campus: e.target.value })}
+        />
+        <TextInput
+          placeholder="Modalidade:"
+          value={studentObj.mode}
+          onChange={(e) => setStudent({ ...studentObj, mode: e.target.value })}
+        />
+        <TextInput
+          placeholder="Área:"
+          value={studentObj.area}
+          onChange={(e) => setStudent({ ...studentObj, area: e.target.value })}
+        />
+        <TextInput
+          placeholder="Curso:"
+          value={studentObj.course}
+          onChange={(e) => setStudent({ ...studentObj, course: e.target.value })}
+        />
         <Button text="Cadastrar" />
         <a href="/">ou login</a>
       </form>
