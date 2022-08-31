20eefb9e074da91be5e568a8ff36f5ed9fc921c5
unamednada <dias.gustavo.dias@gmail.com>
2022-08-31 14:06:08 -0300

    feat: create header skeleton

---
+++ src/templates/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <div>
+      <div>
+        <img src="logo.svg" alt="profile_pic" />
+        <h1>Aluno fulano de tal</h1>
+        <h3>Curso e coisa e tal</h3>
+      </div>
+      <div>
+        <h2>Cidade, Estado</h2>
+        <h2>Semestre</h2>
+        <h2>e-mail</h2>
+      </div>
+    </div>
+  );
+}
