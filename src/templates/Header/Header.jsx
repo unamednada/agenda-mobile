179de60438c5ccf993248746b4cab914da412f23
DanielStark <kamirates@gmail.com>
2022-09-12 09:15:15 -0300

    add style header

--- src/templates/Header/Header.jsx
+++ src/templates/Header/Header.jsx
@@ -10,16 +10,15 @@ export default function Header() {
       <div class="main">
         <header>
           <div class="logo">
-            <h1><img src="https://924234.smushcdn.com/2329743/wp-content/uploads/2022/05/netflix-distribui-nfts-da-serie-love-death-and-robots-saiba-como-ganhar.jpg?lossy=1&strip=1&webp=1" alt="perfil" />{nome}</h1>
-
+            <h1><a href="/"><img src="https://924234.smushcdn.com/2329743/wp-content/uploads/2022/05/netflix-distribui-nfts-da-serie-love-death-and-robots-saiba-como-ganhar.jpg?lossy=1&strip=1&webp=1" alt="perfil" /></a>{nome}</h1>
           </div>
-
+         
           <nav>
             <ul class="container">
-              <li class="item"><h3>{curso}</h3></li>
-              <li class="item"><h2>{modalidade}</h2></li>
-              <li class="item"><h2>{email}</h2></li>
-              <li class="item"><h2>{cidade}</h2></li>
+              <li class="item"><a>{curso}</a></li>
+              <li class="item"><a>{modalidade}</a></li>
+              <li class="item"><a>{email}</a></li>
+              <li class="item"><a>{cidade}</a></li>
             </ul>
           </nav>
         </header>
